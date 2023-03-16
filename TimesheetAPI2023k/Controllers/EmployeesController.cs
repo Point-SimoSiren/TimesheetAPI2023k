30406cf657ee02c4318887174590ba1022355d57
Simo Sirén <simo.siren1@careeria.fi>
2023-03-16 13:19:11 +0200

    Add project files.

---
+++ TimesheetAPI2023k/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimesheetAPI2023k.Models;
+
+namespace TimesheetAPI2023k.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private tuntidbContext db = new tuntidbContext();
+
+        [HttpGet]
+        public ActionResult GetAllActiveEmployees()
+        {
+            try
+            {
+                var emp = (from e in db.Employees where e.Active == true select e).ToList();
+
+                return Ok(emp);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error happened: " + e);
+            }
+        }
+    }
+}
